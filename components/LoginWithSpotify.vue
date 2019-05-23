eb4357ab5ee13e9cb9ac92afd5a693ff33167a81
Santiago Martín Agra <santiagomartinagra@gmail.com>
2019-05-23 22:03:12 +0200

    build: use "NUXT_ENV_" prefix for enviroment variables

--- components/LoginWithSpotify.vue
+++ components/LoginWithSpotify.vue
@@ -12,8 +12,8 @@ import Vue from 'vue';
 export default Vue.extend({
   computed: {
     authUrl() {
-      return `https://accounts.spotify.com/authorize?response_type=token&client_id=${
-        process.env.CLIENT_ID
+      return `https://accounts.spotify.com/authorize?response_type=token&NUXT_ENV_CLIENT_ID=${
+        process.env.NUXT_ENV_CLIENT_ID
       }&scope=user-read-currently-playing&redirect_uri=${encodeURIComponent(
         location.href
       )}`;
