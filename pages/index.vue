f8b254c8e718b1cf914277d28d50942f99829fa4
Santiago Martín Agra <santiagomartinagra@gmail.com>
2019-05-23 22:41:25 +0200

    feat: Information about the app

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,21 @@
 <template>
-  <section class="w-full h-screen items-center justify-center flex bg-black">
-    <LoginWithSpotify v-if="showLogin" />
+  <section
+    class="w-full h-screen items-center justify-center flex flex-col bg-black"
+  >
+    <div
+      v-if="showLogin"
+      class="text-white flex items-center justify-center flex-col"
+    >
+      <h1 class="text-3xl">🎵 Current song 🎵</h1>
+      <p class="ml-8 mr-8 pt-4 pb-4">
+        Login with Spotify and get an URL you can add to OBS (or other streaming
+        programs) to show your audiciente your current Spotify song.
+      </p>
+      <LoginWithSpotify />
+      <footer class="text-white pt-4">
+        Made with ❤ by <a href="https://santiagomartin.dev">@SantiMA10</a>
+      </footer>
+    </div>
     <CurrentSong v-else :song="song" />
   </section>
 </template>
