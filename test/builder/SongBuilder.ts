949d3521a75e1d0391085ab4d3f284084b691ba5
Santiago Martín Agra <santiagomartinagra@gmail.com>
2019-05-27 22:01:35 +0200

    test: add album cover test in 'CurrentSong'

--- test/builder/SongBuilder.ts
+++ test/builder/SongBuilder.ts
@@ -1,20 +1,29 @@
+import { merge } from 'lodash';
+import { DeepPartial } from 'utility-types';
 import { Song } from '~/entities/Song';
 
 export class SongBuilder {
+  protected instance: Song = {
+    name: "I'm a song",
+    artists: [
+      {
+        name: 'Artist'
+      }
+    ],
+    album: {
+      images: [],
+      name: 'Album'
+    },
+    progress_ms: 0,
+    duration_ms: 0
+  };
+
+  public with(song: DeepPartial<Song>) {
+    this.instance = merge(this.instance, song);
+    return this;
+  }
+
   public getInstance(): Song {
-    return {
-      name: "I'm a song",
-      artists: [
-        {
-          name: 'Artist'
-        }
-      ],
-      album: {
-        images: [],
-        name: 'Album'
-      },
-      progress_ms: 0,
-      duration_ms: 0
-    };
+    return this.instance;
   }
 }
--- test/unit/components/CurrentSong.spec.ts
+++ test/unit/components/CurrentSong.spec.ts
@@ -2,6 +2,7 @@ import { mount } from '@vue/test-utils';
 
 import CurrentSong from '~/components/CurrentSong.vue';
 import { SongBuilder } from '~/test/builder/SongBuilder';
+import { AlbumCover } from '~/entities/AlbumCover';
 
 describe('CurrentSong', () => {
   it('renders', () => {
@@ -12,5 +13,33 @@ describe('CurrentSong', () => {
     });
 
     expect(subject.isVueInstance()).toBeTruthy();
+  });
+
+  it('not renders the album cover if there is images', () => {
+    const subject = mount(CurrentSong, {
+      propsData: {
+        song: new SongBuilder().getInstance()
+      }
+    });
+
+    expect(subject.find('img').exists()).toBeFalsy();
+  });
+
+  it('renders the album cover with width between 64 and 400 pixels', () => {
+    const images: AlbumCover[] = [
+      { url: 'https://picsum.photos/10', width: 10, height: 10 },
+      { url: 'https://picsum.photos/200', width: 200, height: 200 }
+    ];
+
+    const subject = mount(CurrentSong, {
+      propsData: {
+        song: new SongBuilder().with({ album: { images } }).getInstance()
+      }
+    });
+
+    const image = subject.find('img');
+
+    expect(image).not.toBeNull();
+    expect(image.attributes('src')).toEqual('https://picsum.photos/200');
   });
 });
