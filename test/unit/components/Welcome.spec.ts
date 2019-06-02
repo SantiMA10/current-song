6fb8a5ce0018f341431c597dc87717377e1787b8
Santiago Martín Agra <santiagomartinagra@gmail.com>
2019-06-02 21:03:30 +0200

    test: fix snapshop testing

--- test/unit/components/Welcome.spec.ts
+++ test/unit/components/Welcome.spec.ts
@@ -1,9 +1,9 @@
-import { mount } from '@vue/test-utils';
+import { shallowMount } from '@vue/test-utils';
 import Welcome from '~/components/Welcome.vue';
 
 describe('Welcome', () => {
   it('renders', () => {
-    const subject = mount(Welcome);
+    const subject = shallowMount(Welcome);
 
     expect(subject.element).toMatchSnapshot();
   });
